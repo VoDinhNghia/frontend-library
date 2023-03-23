546f1e104b89407818572f6bc63518b0a1ee7856
VoDinhNghia <vodinhnghia85@gmail.com>
2023-03-23 20:22:24 +0700

    Update code admin dashboard and modal edit library

--- src/components/admin/dashboardAdmin.jsx
+++ src/components/admin/dashboardAdmin.jsx
@@ -1,185 +1,22 @@
 import React, { Component } from "react";
-import libraryService from "../../services/libraryService";
-import userService from "../../services/userService";
-import { BsPencil } from "react-icons/bs";
-import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
-import "./index.css";
-import moment from "moment/moment";
-
+import MenuAdmin from "./components/MenuAdmin";
+import LibrarySetting from "./components/Library";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 export default class BoardAdmin extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      libraryInfo: {},
-      showModal: false,
-      nameLibrary: "",
-      year: "",
-      userList: [],
-      librarian: "",
-      description: "",
-    };
-  }
-
-  componentDidMount() {
-    this.getInfoLibrary();
-    this.getUserList();
-  }
-
-  getInfoLibrary() {
-    libraryService.getInfoLibrary().then((response) => {
-      this.setState({
-        libraryInfo: response?.data?.data,
-      });
-    });
-  }
-
-  getUserList() {
-    userService.getAllUsers().then((res) => {
-      this.setState({
-        userList: res.data?.data,
-      });
-    });
-  }
-
-  updateField(value, field) {
-    switch (field) {
-      case "name":
-        this.setState({
-          nameLibrary: value.target.value,
-        });
-        break;
-      case "year":
-        this.setState({
-          year: value.target.value,
-        });
-        break;
-      case "librarian":
-        this.setState({
-          librarian: value.target.value,
-        });
-        break;
-      case "description":
-        this.setState({
-          description: value.target.value,
-        });
-        break;
-      default:
-        break;
-    }
-  }
-
-  handleClose() {
-    this.setState({
-      showModal: false,
-    });
-  }
-
-  handleShow() {
-    this.setState({
-      showModal: true,
-    });
-  }
-
-  async handleSubmit() {
-    const {
-      nameLibrary,
-      year,
-      libraryInfo = {},
-      librarian,
-      description,
-    } = this.state;
-    const id = libraryInfo._id || "";
-    const updateBody = {
-      name: nameLibrary || libraryInfo.name,
-      foundYear: year || libraryInfo.foundYear,
-      librarian: librarian || libraryInfo.librarian?.id,
-      description: description || libraryInfo.description,
-    };
-    await libraryService.updateInfo(id, updateBody)
-    this.setState({
-      showModal: false,
-      libraryInfo: this.getInfoLibrary(),
-    });
+    this.state = {};
   }
 
   render() {
-    const { libraryInfo = {}, showModal, userList = [] } = this.state;
-    const userInfo = userList.map((item) => {
-      return {
-        id: item.id,
-        name: `${item?.lastName} ${item?.firstName}`,
-      };
-    });
     return (
-      <div className="container">
-        <header className="jumbotron">
-          <h3 className="mb-4">
-            Thông tin thư viện
-            <span>
-              <button
-                className="editLibraryInfo"
-                onClick={() => this.handleShow()}
-              >
-                <BsPencil />
-              </button>
-            </span>
-          </h3>
-          <h6>Tên: {libraryInfo.name || ""}</h6>
-          <h6>Năm thành lập: {libraryInfo.foundYear ? moment(libraryInfo.foundYear).format('DD-MM-YYYY') : ''}</h6>
-          <h6>
-            Người quản lý:{" "}
-            {`${libraryInfo?.librarian?.lastName || ''} ${libraryInfo?.librarian?.firstName || ''}`}
-          </h6>
-          <h6>Giới thiệu: {libraryInfo?.description || ""}</h6>
-        </header>
-        <Modal show={showModal} onHide={() => this.handleClose()}>
-          <Modal.Header>
-            <Modal.Title>Cập nhật thông tin thư viện</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <label>Tên: </label>
-            <input
-              className="nameLibrary"
-              type="text"
-              onChange={(value) => this.updateField(value, "name")}
-              defaultValue={libraryInfo.name}
-            />
-            <label>Năm thành lập: </label>
-            <input
-              className="nameLibrary"
-              type="text"
-              onChange={(value) => this.updateField(value, "year")}
-              defaultValue={libraryInfo.foundYear}
-            />
-            <label>Người quản lý:</label>
-            <select
-              className="nameLibrary"
-              onChange={(value) => this.updateField(value, "librarian")}
-              defaultValue={libraryInfo.librarian?.id}
-            >
-              {userInfo.map((item) => {
-                return <option value={item.id}>{item.name}</option>;
-              })}
-            </select>
-            <label>Giới thiệu: </label>
-            <textarea
-              className="nameLibrary"
-              type="text"
-              onChange={(value) => this.updateField(value, "description")}
-              defaultValue={libraryInfo.description}
-            />
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="danger" onClick={() => this.handleClose()}>
-              Hủy
-            </Button>
-            <Button variant="primary" onClick={() => this.handleSubmit()}>
-              Cập nhật
-            </Button>
-          </Modal.Footer>
-        </Modal>
+      <div>
+        <Row>
+          <Col xs lg="2"><MenuAdmin /></Col>
+          <Col style={{ marginLeft: 15 }}><LibrarySetting /></Col>
+        </Row>
       </div>
     );
   }
